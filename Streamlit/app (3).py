6cafb829412817db6f5a1837cad3a2e6443c00a9
Lizaan <lizaanbotha20@gmail.com>
2025-01-06 17:54:00 +0200

    Add files via upload

---
+++ Streamlit/app (3).py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "96a42a72-169a-42b3-a11d-fd0fc9849b1e",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
